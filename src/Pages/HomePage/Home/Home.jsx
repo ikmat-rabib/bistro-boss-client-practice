69f5be43d210554bdba8a611c88360edc4bee169
ikmat-rabib <ikmat.rbb@gmail.com>
2024-03-28 02:42:18 +0600

    finished navbar component

--- src/Pages/HomePage/Home/Home.jsx
+++ src/Pages/HomePage/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Footer from "../../Shared/Footer/Footer";
+import Navbar from "../../Shared/Navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <h2>this is home</h2>
             <Footer></Footer>
         </div>
